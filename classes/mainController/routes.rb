cad298f0fd3dd13caa8902a8dbd58d15ce668c95
mphox <michael.phox@gmail.com>
2013-11-20 17:32:25 -0500

    json finished for the first 5 categories

--- classes/mainController/routes.rb
+++ classes/mainController/routes.rb
@@ -54,6 +54,198 @@ class App < Sinatra::Base
 						}
 					}
 				}
+			},
+			children: {
+				price: {
+					listing_size: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				},
+				views: {
+					view_count: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				}
+			},
+			painting: {
+				price: {
+					listing_size: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				},
+				views: {
+					view_count: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				}
+			},
+			pets: {
+				price: {
+					listing_size: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				},
+				views: {
+					view_count: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				}
+			},
+			hats: {
+				price: {
+					listing_size: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				},
+				views: {
+					view_count: 100,
+					average: 100,
+					standard_dev: 100,
+					histograms: {
+						ten_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10],
+							bin_width: 4
+						},
+						twentyfive_bin:{
+							values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+							bin_width: 4
+						},
+						fifty_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4
+						},
+						onehundred_bin:{
+							values: [1,2,3,4,5,6,7,8,9,0],
+							bin_width: 4							
+						}
+					}
+				}
 			}
 		}.to_json
 	end
