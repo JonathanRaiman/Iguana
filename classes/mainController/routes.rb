2d12f41d6e02ad1d77ebcbdf552fdfcbd045cfa5
mphox <michael.phox@gmail.com>
2013-11-20 17:44:51 -0500

    json added gross_earnings

--- classes/mainController/routes.rb
+++ classes/mainController/routes.rb
@@ -246,6 +246,26 @@ class App < Sinatra::Base
 						}
 					}
 				}
+			},
+			gross_earnings: {
+				histograms: {
+					ten_bin:{
+						values: [1,2,3,4,5,6,7,8,9,10],
+						bin_width: 4
+					},
+					twentyfive_bin:{
+						values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+						bin_width: 4
+					},
+					fifty_bin:{
+						values: [1,2,3,4,5,6,7,8,9,0],
+						bin_width: 4
+					},
+					onehundred_bin:{
+						values: [1,2,3,4,5,6,7,8,9,0],
+						bin_width: 4							
+					}
+				}
 			}
 		}.to_json
 	end
