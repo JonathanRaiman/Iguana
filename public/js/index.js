cf8439277415ba82651926713fefbab7a8ced389
Mitchell Kates <mhkates@gmail.com>
2013-11-20 18:29:10 -0500

    graphs working

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
-var active = "";
+var active = "jewelery";
 
+//****************************************
 //Button functionality to toggle the colors
+//****************************************
 $(document).ready(function() {
 	updateGraphs();
 	$(".button-container a").click(function() {
@@ -9,151 +11,110 @@ $(document).ready(function() {
 				$(handle).removeClass($(handle).attr('data-class'));
 			});
 			$(this).addClass($(this).attr('data-class'));
-			active = $(this).text();
+			active = $(this).text().toLowerCase();
 			updateGraphs();
 		}
 	});
 });
 
 var updateGraphs = function updateGraphs() {
-	//Make JSON Calls
-	$('#graphone').highcharts({
-            title: {
-                text: 'Cool dude',
-                x: -20 //center
-            },
-            xAxis: {
-                categories: ['Handmade', 'Vintage', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: '°C'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }, {
-                name: 'New York',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-            }, {
-                name: 'Berlin',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-            }, {
-                name: 'London',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
-        });
-        $('#graphtwo').highcharts({
-			 chart: {
-                type: 'bar'
-            },
-            title: {
-                text: ''
-            },
-            subtitle: {
-                text: 'Source: Wikipedia.org'
-            },
-            xAxis: {
-                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-                title: {
-                    text: null
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Population (millions)',
-                    align: 'high'
-                },
-                labels: {
-                    overflow: 'justify'
-                }
-            },
-            tooltip: {
-                valueSuffix: ' millions'
-            },
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -40,
-                y: 100,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                name: 'Year 1800',
-                data: [107, 31, 635, 203, 2]
-            }]
-		});
-		$('#graphthree').highcharts({
-            title: {
-                text: '',
-                x: -20 //center
-            },
-            xAxis: {
-                categories: ['Handmade', 'Vintage', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: '°C'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }, {
-                name: 'New York',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-            }, {
-                name: 'Berlin',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-            }, {
-                name: 'London',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
-        });
+	$.ajax({
+		dataType: "json",
+		url: "/data.json",
+		data: {'boxes':25,'types':'views,price','category':active},
+		success: function(response) {
+			var resp = response[active];
+			
+			var price = resp['price'];
+			var price_histogram = price['histograms']['twentyfive_bin'];
+			var price_histogram_values = price_histogram['values'];
+			var bins = generateBins(price_histogram['bin_width'],25);
+			
+			var views = resp['views'];
+			var views_histogram = views['histograms']['twentyfive_bin'];
+			var views_histogram_values = views_histogram['values'];
+
+			$('#graphone').highcharts({
+				chart: {type: 'column'},
+				title: {text: ''},
+				xAxis: {categories: bins,
+					title: {
+						text: 'Price ($)'
+					}
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: 'Quantity'
+					}
+				},
+				tooltip: {
+					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+					footerFormat: '</table>',
+					shared: true,
+					useHTML: true
+				},
+				plotOptions: {
+					column: {
+						pointPadding: 0.2,
+						borderWidth: 0
+					}
+				},
+				legend: {
+					enabled: false,
+				},
+				series: [{
+					name: '',
+					data: price_histogram_values
+				}]
+			});
+			$('#graphthree').highcharts({
+				chart: {type: 'column'},
+				title: {text: ''},
+				xAxis: {categories: bins,
+					title: {
+						text: 'Number of Views'
+					}
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: 'Quantity'
+					}
+				},
+				tooltip: {
+					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+					footerFormat: '</table>',
+					shared: true,
+					useHTML: true
+				},
+				plotOptions: {
+					column: {
+						pointPadding: 0.2,
+						borderWidth: 0
+					}
+				},
+				legend: {
+					enabled: false,
+				},
+				series: [{
+					name: '',
+					data: views_histogram_values
+				}]
+			});
+		}
+	});
+};
+
+
+var generateBins = function generateBins(width,number) {
+	list = [];
+	for (i = 0; i < width*number; i = i+width) {
+		list.push(i)
+	}
+	return list;
 };
