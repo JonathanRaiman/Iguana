57de9908dfaa069064c5fd9792f3570376f01f47
Jonathan Raiman <jonathanraiman@gmail.com>
2013-11-21 13:16:32 -0500

    changes

--- classes/Etsy/Etsy.rb
+++ classes/Etsy/Etsy.rb
@@ -2,6 +2,6 @@ require 'mongo_mapper'
 require "etsy"
 
 MongoMapper.database   = "iguana"
-MongoMapper.connection = Mongo::Connection.new("localhost",nil, :pool_size => 10, :pool_timeout => 30)
+MongoMapper.connection = Mongo::Connection.new((ENV['MONGOHQ_URL'] || "localhost"),nil, :pool_size => 10, :pool_timeout => 30)
 
 %w(Listing Shop User configuration ShopSearchMethods).map {|d| require_relative(d)}
