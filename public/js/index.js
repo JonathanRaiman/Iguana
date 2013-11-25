4c551a7b6f4cfe239e428e80350f6e000f0cf0b3
Mitchell Kates <mhkates@gmail.com>
2013-11-25 13:50:34 -0500

    adding carousel

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,20 @@ var active = "";
 //****************************************
 $(document).ready(function() {
 	//updateGraphs();
-	$(".button-container a").click(function() {
-		if ($(this).text() != active) {
-			$.each($('.button-container a'),function(index,handle) {
-				$(handle).removeClass($(handle).attr('data-class'));
-			});
-			$(this).addClass($(this).attr('data-class'));
-			active = $(this).text();
-			updateGraphs();
-		}
-	});
+	// $(".button-container a").click(function() {
+// 		if ($(this).text() != active) {
+// 			$.each($('.button-container a'),function(index,handle) {
+// 				$(handle).removeClass($(handle).attr('data-class'));
+// 			});
+// 			$(this).addClass($(this).attr('data-class'));
+// 			active = $(this).text();
+// 			updateGraphs();
+// 		}
+// 	});
+	$('.carousel').carousel({
+	  interval: 500
+	})
+
 });
 
 var updateGraphs = function updateGraphs() {
@@ -40,78 +44,43 @@ var updateGraphs = function updateGraphs() {
 			var views_histogram = views['data'];
 			var bins = generateBins(views['fork_size'],views_histogram.length);
 
-			$('#graphone').highcharts({
-				chart: {type: 'column'},
-				title: {text: ''},
-				xAxis: {categories: bins,
-					title: {
-						text: 'Price ($)'
-					}
-				},
-				yAxis: {
-					min: 0,
-					title: {
-						text: 'Quantity'
-					}
-				},
-				tooltip: {
-					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-					footerFormat: '</table>',
-					shared: true,
-					useHTML: true
-				},
-				plotOptions: {
-					column: {
-						pointPadding: 0.2,
-						borderWidth: 0
-					}
-				},
-				legend: {
-					enabled: false,
-				},
-				series: [{
-					name: '',
-					data: price_histogram
-				}]
-			});
-			$('#graphthree').highcharts({
-				chart: {type: 'column'},
-				title: {text: ''},
-				xAxis: {categories: bins,
-					title: {
-						text: 'Number of Views'
-					}
-				},
-				yAxis: {
-					min: 0,
-					title: {
-						text: 'Quantity'
-					}
-				},
-				tooltip: {
-					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-					footerFormat: '</table>',
-					shared: true,
-					useHTML: true
-				},
-				plotOptions: {
-					column: {
-						pointPadding: 0.2,
-						borderWidth: 0
-					}
-				},
-				legend: {
-					enabled: false,
-				},
-				series: [{
-					name: '',
-					data:views_histogram
-				}]
-			});
+			// $('#graphone').highcharts({
+// 				chart: {type: 'column'},
+// 				title: {text: ''},
+// 				xAxis: {categories: bins,
+// 					title: {
+// 						text: 'Price ($)'
+// 					}
+// 				},
+// 				yAxis: {
+// 					min: 0,
+// 					title: {
+// 						text: 'Quantity'
+// 					}
+// 				},
+// 				tooltip: {
+// 					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+// 					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+// 						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+// 					footerFormat: '</table>',
+// 					shared: true,
+// 					useHTML: true
+// 				},
+// 				plotOptions: {
+// 					column: {
+// 						pointPadding: 0.2,
+// 						borderWidth: 0
+// 					}
+// 				},
+// 				legend: {
+// 					enabled: false,
+// 				},
+// 				series: [{
+// 					name: '',
+// 					data: price_histogram
+// 				}]
+// 			});
+			$.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
 		}
 	});
 };
