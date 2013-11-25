85a89fc679ca8fbdb5b4d0d3f7f39e9954904ade
Jonathan Raiman <jonathanraiman@gmail.com>
2013-11-25 03:06:01 -0500

    fixed queriesg

--- classes/Etsy/Etsy.rb
+++ classes/Etsy/Etsy.rb
@@ -3,10 +3,8 @@ require "etsy"
 require 'uri'
 include Mongodb
 
-# setup_database(ENV['MONGOHQ_URL'] ?
-# 	DatabaseConfiguration.from_url(ENV['MONGOHQ_URL']) :
-# 	DatabaseConfiguration.new(:host => "localhost", :port => nil, :database_name => "iguana"))
-
-setup_database DatabaseConfiguration.from_url("mongodb://heroku:220dee22f49de65f4fe780dd564989ed@paulo.mongohq.com:10051/app19674891")
+setup_database(ENV['MONGOHQ_URL'] ?
+	DatabaseConfiguration.from_url(ENV['MONGOHQ_URL']) :
+	DatabaseConfiguration.new(:host => "localhost", :port => nil, :database_name => "iguana"))
 
 %w(Listing Shop User configuration ShopSearchMethods).map {|d| require_relative(d)}
