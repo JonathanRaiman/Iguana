9f20d1a860891cdbf76ac77ce0d3c9098a2f0197
Jonathan Raiman <jonathanraiman@gmail.com>
2013-12-04 18:30:30 -0500

    new system

--- classes/KPIController/dataAPI.rb
+++ classes/KPIController/dataAPI.rb
@@ -6,6 +6,8 @@ module Sinatra
 				handle_category_stats
 			when "price_stats"
 				handle_price_stats
+			when "view_stats"
+				handle_view_stats
 			else
 				handle_bad_request
 			end
@@ -28,9 +30,52 @@ module Sinatra
 			categories.to_json
 		end
 
+		def return_histograms
+			resp = {
+			category: params[:_id],
+			series: {}
+			}
+			@types.each do |type|
+				resp[:series][type["name"].to_sym] = {
+					type: type["name"],
+					data: hists[type["name"].to_sym].histogram,
+					fork_size: hists[type["name"].to_sym].fork_size,
+					min_value: hists[type["name"].to_sym].min_value,
+					max_value: hists[type["name"].to_sym].max_value
+				}
+			end
+			resp.to_json
+		end
+
+		def create_histogram_from_types
+			@hists = @listing_count.histogram :types => @types
+		end
+
+		def handle_view_stats
+			if find_ListingCount
+				@types = [create_type_from_request("views")]
+				create_histogram_from_types
+				return_histograms
+			else
+				handle_bad_request
+			end
+		end
+
+		def create_type_from_request name
+			type = {"name" => name}
+			if !params["min_value"].nil? then type.merge!({"min_value" => params["min_value"].to_f}) end
+			if !params["max_value"].nil? then type.merge!({"max_value" => params["max_value"].to_f}) end
+			type
+		end
+
 		def handle_price_stats
-			#uh-oh
-			handle_bad_request
+			if find_ListingCount
+				@types = [create_type_from_request("price")]
+				create_histogram_from_types
+				return_histograms
+			else
+				handle_bad_request
+			end
 		end
 
 		def handle_bad_request
